0524051fbd25b7b26736c101d40c083e27436860
shubhanshusarkar <shubhanshu.sarkar@hcl-software.com>
2026-07-30 23:34:46 +0530

    Create claims.sql

---
+++ claims.sql
@@ -0,0 +1,30 @@
+INSERT INTO claims VALUES
+('CLM0001','PH0001','2026-01-12',48000,'PROV001','DEV001',
+'Rear collision with repeated repair invoices'),
+
+('CLM0002','PH0002','2026-01-15',62000,'PROV001','DEV002',
+'Medical reimbursement with inflated bills'),
+
+('CLM0003','PH0003','2026-01-17',9500,'PROV003','DEV003',
+'Windshield replacement'),
+
+('CLM0004','PH0004','2026-01-19',81000,'PROV002','DEV001',
+'Large bodily injury settlement'),
+
+('CLM0005','PH0005','2026-01-22',15000,'PROV004','DEV004',
+'Water damage after pipe leakage'),
+
+('CLM0006','PH0006','2026-01-24',72000,'PROV001','DEV005',
+'Multiple surgery invoices'),
+
+('CLM0007','PH0007','2026-01-26',18000,'PROV005','DEV006',
+'Minor collision'),
+
+('CLM0008','PH0008','2026-01-28',56000,'PROV002','DEV001',
+'Repeated accident with same garage'),
+
+('CLM0009','PH0009','2026-01-30',43000,'PROV006','DEV007',
+'Fire damage'),
+
+('CLM0010','PH0010','2026-02-02',92000,'PROV001','DEV002',
+'Total vehicle loss');
